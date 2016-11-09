c74efd8f2d5ec28c6e7805dc57e44d0799763975
Francois Catuhe <fcatuhe@gmail.com>
2016-11-09 19:23:27 +0100

    solve embedded reviews in restaurants show

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,7 +5,7 @@ class Restaurant < ApplicationRecord
   validates :address, presence: true
 
   def stars
-    ratings = reviews.map { |review| review.rating}
+    ratings = reviews.map { |review| review.rating}.select { |rating| !rating.nil? }
     rating = ratings.size > 0 ? ratings.sum.fdiv(ratings.size).round : 0
     ('<i class="fa fa-star" aria-hidden="true"></i>' * rating + '<i class="fa fa-star-o" aria-hidden="true"></i>' * (5 - rating)).html_safe
   end
